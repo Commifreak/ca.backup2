d0ceb7bc7d4f048be976b8bd231a003888dc85a8
Robin Kluth <kontakt@kluthr.de>
2022-12-02 13:56:19 +0100

    Adjustments

--- source/ca.backup2/pkg_build.sh
+++ source/ca.backup2/pkg_build.sh
@@ -7,10 +7,10 @@ version=$(date +"%Y.%m.%d")$1
 
 mkdir -p $tmpdir
 
-cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
+cp --parents -f $(find . -type f ! \( -iname "pkg-build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
 cd $tmpdir
-makepkg -l y -c y ${archive}/${plugin}-${version}-x86_64-1.txz
+makepkg -l y -c y /tmp/${plugin}-${version}-x86_64-1.txz
 rm -rf $tmpdir
 echo "MD5:"
-md5sum ${archive}/${plugin}-${version}-x86_64-1.txz
+md5sum /tmp/${plugin}-${version}-x86_64-1.txz
 
